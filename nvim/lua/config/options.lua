a375fe974d7a8d05801c78e56c5ad31d8bba8795
57young <61356621+57young@users.noreply.github.com>
2025-05-22 18:22:06 +0800

    Delete nvim directory

--- nvim/lua/config/options.lua
+++
@@ -1,9 +0,0 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-
--- rainbow-delimiters
-vim.opt.termguicolors = true
-vim.opt.showmatch = true
-vim.opt.matchtime = 2
-vim.opt.clipboard = "unnamedplus"
